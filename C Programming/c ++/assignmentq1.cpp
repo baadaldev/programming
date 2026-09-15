77fc35e75cae64a9694fa387c5ac9e62aa755ef9
Baadal89131 <badolrakib1@gmail.com>
2026-09-15 15:32:55 +0600

    Variables and data types assignment question 1

---
+++ C Programming/c ++/assignmentq1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main (){
+int n;
+cout<<"Enter the number of sides: ";
+cin>>n;
+int area= n*n;
+cout<<"Area of square is: "<<area<<endl;
+    
+    return 0;
+}
