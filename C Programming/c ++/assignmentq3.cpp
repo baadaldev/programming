f3b97aeac9af902aba417b676bc23a9329449ff8
Baadal89131 <badolrakib1@gmail.com>
2026-09-15 17:22:03 +0600

    Constant

--- C Programming/c ++/assignmentq3.cpp
+++ C Programming/c ++/assignmentq3.cpp
@@ -1,15 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main (){
-float p,r,t;
-cout<<"Enter the principal amount: ";
-cin>>p;     
-cout<<"Enter the rate of interest: ";
-cin>>r;
-cout<<"Enter the time in years: ";
-cin>>t;
-float simple_interest=(p*r*t)/100;
-cout<<"Simple interest is: "<<simple_interest<<endl;
-    
+int main()
+{
+    float p, r, t;
+    cout << "Enter the principal amount: ";
+    cin >> p;
+    cout << "Enter the rate of interest: ";
+    cin >> r;
+    cout << "Enter the time in years: ";
+    cin >> t;
+    float simple_interest = (p * r * t) / 100;
+    cout << "Simple interest is: " << simple_interest << endl;
+
     return 0;
 }
---
+++ C Programming/c ++/constant.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main (){
+const int a=10;
+const int b=20;
+cout<<"value of a is "<<a<<endl;
+cout<<"value of b is "<<b<<endl;
+    
+    return 0;
+}
