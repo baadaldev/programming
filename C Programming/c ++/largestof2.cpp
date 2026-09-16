66378c9e9bdf0c63945bfd3dbc4c3381e1dc4c6d
Baadal89131 <badolrakib1@gmail.com>
2026-09-17 04:57:24 +0600

    Largest

---
+++ C Programming/c ++/largestof2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int a, b;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    if(a>b){
+        cout<<"Largest number is: "<<a<<endl;
+    }
+    else{
+        cout<<"Largest number is: "<<b<<endl;
+    }
+}
