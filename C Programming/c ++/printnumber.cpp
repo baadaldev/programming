949ee0a0eefc0c70368269b5995ec0235b146908
Baadal89131 <badolrakib1@gmail.com>
2026-09-24 06:32:28 +0600

    Add program to print a message a specified number of times

---
+++ C Programming/c ++/printnumber.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int i;
+    int n;
+    cout<<"Enter the number of times you want to print your name: ";
+    cin>>n;
+    for (i=0;i<n;i++){
+        cout<<i+1<<endl;
+    }
+}
