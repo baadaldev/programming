284bffc20cee110305208adc7bde8b3a64b71b73
Baadal89131 <badolrakib1@gmail.com>
2026-09-25 08:10:44 +0600

    Add program to find the index of a target number in an array

---
+++ C Programming/Algorithm/leetcoadprblm.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+   int n,target;
+   scanf("%d",&n);
+   int arr[n];
+   printf("Nums=");
+   for(int i=0;i<n;i++){
+    scanf("%d",arr[i]);
+   }
+   scanf("%d",&target);
+    for(int i=0;i<n;i++){
+    if(arr[i]==target)
+    printf("%d",i);
+    break;
+   }
+   if
+   {
+    return -1;
+   }
+
+
+    return 0;
+}
